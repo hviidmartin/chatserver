754b3b04d4380eacf7131323b766ce9e0e68458f
Martin Hviid <hviidmartin@gmail.com>
2020-10-28 12:00:17 +0100

    asd

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    databaseURI: 'mongodb+srv://hviid:d6xoqal0@hviid.z6cnm.mongodb.net/chatserverDB?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
+    databaseURI: 'mongodb+srv://hviid:d6xoqal0@hviid.z6cnm.mongodb.net/chatserverDB?authSource=admin&replicaSet=atlas-zlkpjx-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', // or MongoDB Atlas connection URI
     localPort: 8080
 };
 
